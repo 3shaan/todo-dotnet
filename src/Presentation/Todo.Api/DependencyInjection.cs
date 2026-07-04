cb22ff11468c209d9ec0c0c59aebd752b59c1764
Eshan <3shaan.dev@proton.me>
2026-07-04 21:49:27 +0600

    Add Dependency Injection for TodoDbContext and update appsettings for connection string

---
+++ src/Presentation/Todo.Api/DependencyInjection.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.Infra.Persistance.Entities;
+
+namespace Todo.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        var connectionString = configuration.GetConnectionString("TodoAppDB");
+
+        services.AddDbContext<TodoDbContext>(options => options.UseSqlite(connectionString));
+
+
+        return services;
+
+    }
+
+}
